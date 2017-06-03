e35e60bfc391f470b7222115553068f15b00e94a
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-06-03 14:27:23 +0300

    Data-set reformat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from numpy import genfromtxt
 from pprint import pprint
 
-my_data = genfromtxt('data.csv', delimiter=';', dtype=None)
+data = genfromtxt('data.csv', delimiter=',', dtype=None)
 
-pprint(my_data[0][0])
+pprint(data)
