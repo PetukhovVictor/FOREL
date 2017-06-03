9aa9ac5659107d2c8d2e1c03aa76825015b155c2
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-06-03 16:23:29 +0300

    Get same objects implementation.

--- forel.py
+++ forel.py
@@ -1,7 +1,14 @@
 import numpy as np
 import random
 
+from scipy.spatial.distance import cdist
+
+from pprint import pprint
+
 class Forel:
+    RADIUS = 10
+    SLICE_OBJECT = slice(1, 5)
+
     def __init__(self, data):
         self.data = data
 
@@ -15,3 +22,13 @@ class Forel:
 
     def clustering_not_finish(self):
         return len(self.data) != 0
+
+    def get_same_objects(self, object):
+        same_objects = []
+        first_object = np.asarray(tuple(object))[self.SLICE_OBJECT].reshape(1, -1)
+        for row in self.data:
+            second_object = np.asarray(tuple(row))[self.SLICE_OBJECT].reshape(1, -1)
+            distance = cdist(first_object, second_object, 'euclidean')
+            if distance <= self.RADIUS:
+                same_objects.append(row)
+        return same_objects
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from numpy import genfromtxt
 from pprint import pprint
 
-
 from forel import Forel
 
 data = genfromtxt('data.csv', delimiter=',', dtype=None, skip_header=True)
@@ -10,4 +9,6 @@ forel = Forel(data)
 
 while forel.clustering_not_finish():
     currently_object = forel.get_rand_object()
-    pprint(currently_object)
+    same_objects = forel.get_same_objects(currently_object)
+    pprint(same_objects)
+    pprint('----------------------')
