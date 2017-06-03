cc9a4d4608d24d8094652fe01c2f2007e23ec44f
Petukhov Victor <VAPetukhov.SBT@sberbank.ru>
2017-06-03 16:38:30 +0300

    Is written pydoc

--- forel.py
+++ forel.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import random
 
@@ -6,12 +8,16 @@ from scipy.spatial.distance import cdist
 from pprint import pprint
 
 class Forel:
+    """ Радиус сферы, внутри которой объекты являются похожими (одного кластера). """
     RADIUS = 10
+
+    """ Срез для измерений кортежа выборки, которые учавствуют в расчетах схожести и центра масс. """
     SLICE_OBJECT = slice(1, 5)
 
     def __init__(self, data):
         self.data = data
 
+    """ Получение и извлечение случайного объекта из выборки. """
     def get_rand_object(self):
         if len(self.data) == 0:
             return None
@@ -20,9 +26,11 @@ class Forel:
         self.data = np.delete(self.data, index, 0)
         return rand_object
 
+    """ Проверка наличия ещё некластеризованных объектов. """
     def clustering_not_finish(self):
         return len(self.data) != 0
 
+    """ Получение похожих объектов. Критерий похожести - расстояние в Евклидовом пространстве. """
     def get_same_objects(self, object):
         same_objects = []
         first_object = np.asarray(tuple(object))[self.SLICE_OBJECT].reshape(1, -1)
@@ -32,3 +40,9 @@ class Forel:
             if distance <= self.RADIUS:
                 same_objects.append(row)
         return same_objects
+
+    """ Получение похожих объектов. Критерий похожести - расстояние в Евклидовом пространстве. """
+    def get_mass_center(self, objects):
+
+        return None
+
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+# -*- coding: utf-8 -*-
+
 from numpy import genfromtxt
 from pprint import pprint
 
 from forel import Forel
 
+# Загружаем data-set.
 data = genfromtxt('data.csv', delimiter=',', dtype=None, skip_header=True)
 
 forel = Forel(data)
 
 while forel.clustering_not_finish():
+    # Извлекаем случайный элемент из выборки.
     currently_object = forel.get_rand_object()
+
+    # Получаем похоже на него объекты (находящиеся на расстоянии менее заданного).
     same_objects = forel.get_same_objects(currently_object)
+
+    # Расчитываем центр масс полученного набора похожих объектов.
+    center_object = forel.get_mass_center(same_objects)
+
     pprint(same_objects)
     pprint('----------------------')
